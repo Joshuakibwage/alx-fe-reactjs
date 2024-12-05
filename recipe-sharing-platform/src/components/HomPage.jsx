a102485591be3df6ce3de9779954aab97e570f73
Josh <joshuakibwage23@gmail.com>
2024-12-05 12:06:27 +0300

    Initial setup and home page creation for Recipe Sharing Platform

--- recipe-sharing-platform/src/components/HomPage.jsx
+++ recipe-sharing-platform/src/components/HomPage.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 
 const HomPage = () => {
+
+    const [recipes, setRecipes] = useState();
+
+    useEffect(() => {
+      fetch("/src/data.json")
+      .then((response) => response.json())
+      .then((data) => setRecipes(data))
+      .catch((error) => console.error("Error fetching recipes:", error));
+    }, []);
+    
   return (
-    <div>HomPage</div>
+    <div>
+      <h1>Recipes Sharing Platform</h1>
+      <div>
+        {recipes.map((recipe) => (
+          <div  key={recipe.id}>
+              <img 
+              src={recipe.image} 
+              alt={recipe.title} 
+              />
+              <h2>{recipe.title}</h2>
+              <p>{recipe.summary}</p>
+          </div>
+        ))}
+      </div>
+
+    </div>
   )
 }
 
