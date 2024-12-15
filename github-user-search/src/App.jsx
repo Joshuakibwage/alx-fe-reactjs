881950006060c2d8ab55dba96c409340db9dd8b1
Josh <joshuakibwage23@gmail.com>
2024-12-15 10:48:14 +0300

    Add githubuser.js file to handle api requests

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,13 @@
-import SearchInput from "./components/Search";
 
-function App() {
+import Search from './components/Search';
+
+const App = () => {
   return (
-    <div className="App">
-      <SearchInput />
+    <div>
+      <h1>GitHub User Search</h1>
+      <Search />
     </div>
   );
-}
+};
 
 export default App;
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,9 +1,8 @@
- 
+ import axios from "axios"
 
-const githubService = () => {
-  return (
-    <div>githubService</div>
-  )
+export const fetchUserData = async(username) => {
+    try{const response = await axios.get(`https://api.github.com/users/${username}`);}
+    return response.data;
+}catch(error){
+    throw error;
 }
-
-export default githubService
