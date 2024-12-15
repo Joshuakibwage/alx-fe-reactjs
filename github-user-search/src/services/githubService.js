d84cdcfef8b150ab5699d16a1634272de1bb49a7
Josh <joshuakibwage23@gmail.com>
2024-12-15 15:55:46 +0300

    Enhance  user interface and advanced styling

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-export const fetchUserData = async (username, location = '', repos = '') => {
+export const fetchUserData = async (username, location = '', repos = '', minRepos = '') => {
   try {
     let query = `https://api.github.com/search/users?q=${username}`;
     if (location) query += `+location:${location}`;
     if (repos) query += `+repos:>=${repos}`;
+    if (minRepos) query += `+repos:>=${minRepos}`;
 
     const response = await axios.get(query);
     return response.data.items;
